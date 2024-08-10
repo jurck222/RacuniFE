fce773807d562d709211bf633fd83276f54a6de6
jurck222 <56190152+jurck222@users.noreply.github.com>
2024-08-10 15:57:10 +0200

    :recycle: Added popover for more info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 import { InvoiceComponentComponent } from './components/invoice-component/invoice-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InvoiceComponentComponent],
+  imports: [RouterOutlet, InvoiceComponentComponent, NgbPopover],
   template: `
     <div class="container mt-5">
-      <div class="row">
+      <div class="text-center">
+        <h1>Računi</h1>
+        <button
+          type="button"
+          title="klikni za info"
+          class="unstyled-button"
+          placement="bottom"
+          [ngbPopover]="popoverBody"
+          [popoverTitle]="popoverTitle">
+          <i class="fa-solid fa-circle-info"></i>
+        </button>
+      </div>
+      <div class="row mt-5">
         <div class="col-md-6">
           <app-invoice-component />
         </div>
@@ -17,8 +30,15 @@ import { InvoiceComponentComponent } from './components/invoice-component/invoic
         </div>
       </div>
     </div>
+
+    <ng-template #popoverTitle>Pregled računov</ng-template>
+    <ng-template #popoverBody>
+      <p>
+        S klikom na vsrtico v tabeli računov lahko izberete račun in si pogledate njegove informacije na desnem delu
+        ekrana.
+      </p>
+      <p>Računom, ki so označeni z rdečo je potekel rok.</p>
+    </ng-template>
   `,
 })
-export class AppComponent {
-  title = 'Racuni';
-}
+export class AppComponent {}
